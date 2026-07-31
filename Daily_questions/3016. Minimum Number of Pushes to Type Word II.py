f32b1ea75d6340d72d6abed6f6c31bfad8f50ba7
Prince-tech-debug <princeparashar1979@gmail.com>
2026-07-31 23:01:15 +0530

    POTD

---
+++ Daily_questions/3016. Minimum Number of Pushes to Type Word II.py
@@ -0,0 +1,17 @@
+class Solution:
+    def minimumPushes(self, word: str) -> int:
+        res, lev, levels = [j for i,j in Counter(word).items()], [], []
+        res.sort(reverse = True)
+
+        for i in res:
+            lev.append(i)
+            if len(lev) % 8 == 0:
+                levels.append(lev)
+                lev = []
+        levels.append(lev)
+
+        res = 0
+        for i,j in enumerate(levels):
+            res += (sum(j) * (i+1))
+        print(levels)
+        return res
